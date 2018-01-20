3a649b07659d7922315433da638bd734e84825b2
Richard <richard@dqubeglobal.com>
2018-01-20 13:07:01 +0530

    SP used, Top Ten Users By Points (Working)

--- CurrentPointsTableAPI/src/main/java/com/dqube/DAOLayer/UserDAOImpl.java
+++ CurrentPointsTableAPI/src/main/java/com/dqube/DAOLayer/UserDAOImpl.java
@@ -8,7 +8,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.dqube.entity.Current_Points;
+import com.dqube.entity.Current_Points;;
+
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -20,7 +21,7 @@ public class UserDAOImpl implements UserDAO {
 	@Override
 	public List<Current_Points> listTopTen() {
 		
-		String sql = "call sort_by_points()";
+		String sql = "call sort_by_points_current_points()";
 		@SuppressWarnings("deprecation")
 		Query query = sessionFactory.getCurrentSession().createNativeQuery(sql).addEntity(Current_Points.class);
 		List<Current_Points> topten = query.getResultList();
--- CurrentPointsTableAPI/src/main/webapp/WEB-INF/views/userForm.jsp
+++ CurrentPointsTableAPI/src/main/webapp/WEB-INF/views/userForm.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="UTF-8"%>
-<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+    <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
 <head>
